2875dda1e9e555b3385fbb85fb93e75ddf922915
leandro georgis <leandrogeorgis@gmail.com>
2021-08-03 14:08:14 -0500

    version 1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,77 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var google;
+
+interface Marker {
+  position: {
+    lat: number,
+    lng: number,
+  };
+  title: string;
+}
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+
+  map: any;
+  directionsService = new google.maps.DirectionsService();
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  // parque simon bolivar
+  origin = { lat: -1.482700, lng: -78.001027 };
+  // Parque la 93
+  destination = { lat: -1.487125, lng: -78.008437 };
+
 
   constructor() {}
+
+
+  ngOnInit(){
+    this.loadMap();
+  }
+
+  loadMap() {
+    // create a new map by passing HTMLElement
+    const mapEle: HTMLElement = document.getElementById('map');
+    // create map
+    this.map = new google.maps.Map(mapEle, {
+      center: this.origin,
+      zoom: 12
+    });
+  
+    this.directionsDisplay.setMap(this.map);
+  
+    google.maps.event.addListenerOnce(this.map, 'idle', () => {
+      mapEle.classList.add('show-map');
+
+      this.calculateRoute();
+    });
+  }
+
+  private calculateRoute() {
+    this.directionsService.route({
+      origin: this.origin,
+      destination: this.destination,
+      travelMode: google.maps.TravelMode.DRIVING,
+    }, (response, status)  => {
+      if (status === google.maps.DirectionsStatus.OK) {
+        this.directionsDisplay.setDirections(response);
+      } else {
+        alert('Could not display directions due to: ' + status);
+      }
+    });
+    }
+
+
+    addMarker(marker: Marker) {
+      return new google.maps.Marker({
+        position: marker.position,
+        map: this.map,
+        title: marker.title
+      });
+    }
 
 }
